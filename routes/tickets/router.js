01872873a3b6b31c5d6f92590459b0b004bd55db
AmmarKhatri <muhammadammarzia@gmail.com>
2024-02-17 22:25:59 +0500

    completed

--- routes/tickets/router.js
+++ routes/tickets/router.js
@@ -3,9 +3,11 @@ const createTicket = require("./createTIcket");
 const authMiddleware = require("../../controllers/authMiddleware");
 const getTicketsByUser = require("./getTicketsByUser");
 const removeTicket = require("./removeTicket");
+const updateTicket = require("./updateTicket");
 const router = express.Router()
 //ticket route registeration
 router.post("/create", authMiddleware, createTicket);
 router.get("/", authMiddleware, getTicketsByUser);
 router.delete("/:id", authMiddleware, removeTicket);
+router.patch("/:id",authMiddleware, updateTicket);
 module.exports = router;
