e51b21bd0cc4f74f69901842ad8b35e6787236e6
Ammar Khatri <43322268+AmmarKhatri@users.noreply.github.com>
2024-02-18 14:03:56 +0500

    final fixes and collection added

--- routes/tickets/removeTicket.js
+++ routes/tickets/removeTicket.js
@@ -6,8 +6,9 @@ async function removeTicket (req, res) {
         let user = await Users.findOne({ email })
         //user doesnt exist
         if (!user) return res.json({ message: "User doesn't exist!" })
-        const ticket = await Tickets.findOneAndDelete({ _id: req.params.id });
-        user.tickets.pop(ticket._id); // Push ticket reference inside user doc
+        await Tickets.findOneAndDelete({ _id: req.params.id });
+        // Remove ticket ID from user's tickets array
+        user.tickets = user.tickets.filter(ticketId => ticketId.toString() !== req.params.id)
         // Save the updated user document
         await user.save();
         return res.json({ msg: "DELETED" })
--- routes/tickets/updateTicket.js
+++ routes/tickets/updateTicket.js
@@ -1,15 +1,29 @@
 const Tickets = require("../../models/Ticket");
+const Users = require("../../models/User"); // Import the Users model
 
-async function updateTicket(req,res) {
-    try{
-        const ticket = Tickets.findOne({_id: req.params.id})
-        if(!ticket) return res.status(404).json({msg: "Ticket not valid!"})
+async function updateTicket(req, res) {
+    try {
+        const { email } = req.user;
+        let user = await Users.findOne({ email });
+        
+        // User doesn't exist
+        if (!user) return res.status(404).json({ message: "User doesn't exist!" });
+
+        // Fetch ticket
+        const ticket = await Tickets.findOne({ _id: req.params.id });
+
+        // Check if ticket doesn't exist or user doesn't have this ticket
+        if (!ticket || !user.tickets.includes(req.params.id))
+            return res.status(404).json({ msg: "Ticket not valid!" });
+
+        // Update ticket
         ticket.type = req.body.type;
-        ticket.updatedAt = Date.now()
+        ticket.updatedAt = Date.now();
         await ticket.save();
-        return res.status(200).json({msg: "Ticket type updated"})
-    }catch(error){
-        return res.status(500).json({error})
+
+        return res.status(200).json({ msg: "Ticket type updated" });
+    } catch (error) {
+        return res.status(500).json({ error });
     }
 }
 
